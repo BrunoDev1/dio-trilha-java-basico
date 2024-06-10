e170d6dbcf01982af2044852d5979f695f5d46ab
BrunoDev1 <brunoleonardo3@gmail.com>
2024-06-10 08:07:50 -0300

    Tipos de Variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
+import java.util.Date;
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nome = "Bruno";
+        int idade = 34;
+        double peso = 120.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        Date dataNascimento = new Date();
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(peso);
+        System.out.println(sexo);
+
     }
 }
